a621aaaac3e46db1b304519d1cc0483e46e6fafe
Ila Petrovic <ilacpetrovic@gmail.com>
2024-10-01 15:51:23 -0500

    added edges to nodes

--- client/src/TaikoNode.jsx
+++ client/src/TaikoNode.jsx
@@ -1,19 +1,15 @@
 import React from 'react';
 
-const TaikoNode = ({ id, onClick, isSelected }) => {
+const TaikoNode = () => {
   return (
     <div
-      id={id}
-      onClick={onClick}
       style={{
         width: '30px',
         height: '30px',
         borderRadius: '50%',
-        backgroundColor: isSelected ? 'yellow' : 'white',
+        backgroundColor: 'white', // You can change the color or make it dynamic
         display: 'inline-block',
         margin: '5px',
-        cursor: 'pointer',
-        border: '2px solid black',
       }}
     />
   );
