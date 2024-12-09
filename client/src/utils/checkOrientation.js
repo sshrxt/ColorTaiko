85bd598bfaafb78041e5fd6a8bdf2be2a4dcf46f
Maxwell <boyul5@illinois.edu>
2024-12-09 14:38:20 -0600

    fix : fix the logic of flipping orientation

--- client/src/utils/checkOrientation.js
+++ client/src/utils/checkOrientation.js
@@ -58,7 +58,7 @@ export const checkOrientation = (newPair, groupMapRef, topOrientation, botOrient
 
     /*case 4
     */
-    if(topOrientation.current.get(topCombination) && botOrientation.current.get(bottomCombination)) {
+    if (topOrientation.current.get(topCombination) && botOrientation.current.get(bottomCombination)) {
         const topGroup = groupMapRef.current.get(topCombination);
         const bottomGroup = groupMapRef.current.get(bottomCombination);
     
@@ -66,20 +66,21 @@ export const checkOrientation = (newPair, groupMapRef, topOrientation, botOrient
             console.error("One of the groups is missing in groupMapRef");
             return 0;
         }
-
-        if (topGroup === bottomGroup) {
-            return -1;
-        }
+    
         const topDir = topOrientation.current.get(topCombination);
-        console.log(topDir);
         const botDir = botOrientation.current.get(bottomCombination);
-        console.log(botDir);
+    
         const isCrossed = (bottom1 < bottom2 && top1 > top2) || (bottom1 > bottom2 && top1 < top2);
-
-        if (isCrossed) {
-            // 需要翻转 topGroup 中所有 combination 的方向
+        const sameDirection = (topDir === botDir);
+    
+        const shouldFlip = (sameDirection && isCrossed) || (!sameDirection && !isCrossed);
+    
+        if (shouldFlip) {
+            if (topGroup === bottomGroup) {
+                return -1;
+            }
+    
             for (const combo of topGroup.combinations) {
-                console.log(combo);
                 if (topOrientation.current.has(combo)) {
                     const dir = topOrientation.current.get(combo);
                     topOrientation.current.set(combo, dir === "right" ? "left" : "right");
@@ -89,10 +90,10 @@ export const checkOrientation = (newPair, groupMapRef, topOrientation, botOrient
                     botOrientation.current.set(combo, dir === "right" ? "left" : "right");
                 }
             }
-            return 0;
         }
     
         return 0;
     }
+    return 0;    
 };
 
